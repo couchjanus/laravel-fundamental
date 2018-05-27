8c66a1d7dfc5bf301b40d0d4efdf4b7ac0605740
couchjanus <couchjanus@gmail.com>
2018-05-27 13:03:13 +0300

    Add files via upload

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,13 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
+use \Cviebrock\EloquentSluggable\Services\SlugService;
 
 $factory->define(App\Post::class, function (Faker $faker) {
+    $title = $faker->sentence();
     return [
-       'title' => $faker->sentence(),
-       'is_active' => $faker->numberBetween(0, 1),
-       'content' => $faker->paragraph(20),
+        'title' => $title,
+        'slug' => SlugService::createSlug(App\Post::class, 'slug', $title),
+        'is_active' => $faker->numberBetween(0, 1),
+        'content' => $faker->paragraph(20),
         'category_id' => $faker->numberBetween(1, 3),
     ];
 });
-
