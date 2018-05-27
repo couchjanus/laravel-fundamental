1bb544950732147bae250d3ce39a3be46b678ce5
couchjanus <couchjanus@gmail.com>
2018-05-27 13:02:42 +0300

    Add files via upload

--- app/Post.php
+++ app/Post.php
@@ -3,11 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-
-    protected $fillable = ['title', 'content', 'is_active', 'category_id'];
-
+    //
+    use Sluggable;
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 
 }
